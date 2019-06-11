cbd4d78be4211a979ea603005098b8e504cb9fcf
Jesse Howard <jessechoward@gmail.com>
2019-06-11 15:55:33 -0500

    initial POC

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const logger = require('../utils/logging');
 
 router.post('/itsm', (req, res) =>
 {
-	res.status(200).send('Ok');
+	res.status(200).json(req.body);
 
 	logger.debug('headers', req.headers);
 	logger.debug('query', req.query);
